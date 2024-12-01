450dc7468b7341cd50f501b5def4ddf7ef04b50d
Rupesh kumar <112106535+rupesh040@users.noreply.github.com>
2024-12-01 19:22:44 +0530

    Update socket.js

--- config/socket.js
+++ config/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://chat.hyweb.in",
     },});
 
 export function getReceiverSocketId(userId) {
