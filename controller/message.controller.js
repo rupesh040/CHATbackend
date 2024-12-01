580dcfdcc48cab71a5824d32de7bc33555a5f24a
Rupesh kumar <112106535+rupesh040@users.noreply.github.com>
2024-12-01 15:22:50 +0530

    Update message.controller.js

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js"
 import { getReceiverSocketId, io } from "../config/socket.js";
+
+
 export const getUsers = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
@@ -27,8 +29,7 @@ export const getMessages = async (req, res) => {
 }
 export const getAiMessages = async (req, res) => {
     try {
-        const userId = req.user._id;
-        const messages = await Message.find({ $or: [{ senderId: userId, receiverId: userId }, { senderId: userId, receiverId: userId }] })
+        const messages = await Message.find( { $or: [{ post: true}]})
         res.status(200).json(messages)
     } catch (error) {
         console.log(error)
@@ -66,16 +67,49 @@ export const sendMessage = async (req, res) => {
 
 export const sendAiMessage = async (req,res) => {
     try {
-    const { text } = req.body;
+    const { text , image } = req.body;
     const senderId = req.user._id;
     const receiverId = req.user._id;
     const newMessage = new Message({
         senderId,
         receiverId,
-        text
+        text,
+         image,
+        post: true
     });
     await newMessage.save();
     res.status(200).json(newMessage)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+
+
+ export const comment = async (req,res) => {
+    const { comment , postId} = req.body;
+    const userId = req.user._id;
+    try {
+        
+        const messages = await Message.findById(postId);
+        messages.commentUser.push({comment, commentId:userId })
+        await messages.save();
+        res.status(200).json({succes:true, messages})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+export const like = async (req,res) => {
+    const { setlike} = req.body;
+    const userId = req.user._id;
+    try {
+        const like = await Message.findById(setlike);
+        like.likes.push({likeId:userId});
+        await like.save();
+        res.status(200).json({succes:true, like})
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: "Internal server error" })
